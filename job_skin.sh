0099c25027a813fb43d377375d806da3a4570331
sterlie <sara.inbox0@gmail.com>
2026-04-30 14:06:39 +0200

    sh

--- job_skin.sh
+++ job_skin.sh
@@ -3,8 +3,8 @@
 #BSUB -J guided_skin
 #BSUB -n 4
 #BSUB -R "span[hosts=1]"
-#BSUB -R "rusage[mem=8GB]"
-#BSUB -M 10GB
+#BSUB -R "rusage[mem=6GB]"
+#BSUB -M 7GB
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -W 24:00
 #BSUB -u sarste@dtu.dk
